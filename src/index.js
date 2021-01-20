65c7cd8417ecad643643ad2925da01e97436909d
alibek.gao <a.kazhibekov@mail.ru>
2021-01-20 08:33:03 +0200

    init: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array !== 'object') return 0;
+    if(array.length === 0) return 0;
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array !== 'object') return 0;
+    if(array.length === 0) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(typeof array !== 'object') return 0;
+    if(array.length === 0) return 0;
+    return array.reduce( ( p, c ) => p + c, 0 ) / array.length;
 }
